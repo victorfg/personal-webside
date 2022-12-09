c9c31359d45db312c04412b11c03e25f960ddde4
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2022-12-09 10:17:18 +0100

    analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import TinaProvider from '../.tina/components/TinaDynamicProvider'
 import LayoutWrapper from '../components/LayoutWrapper'
+import Script from 'next/script'
 import '../styles/tailwind.css'
 import '../styles/custom.css'
 
@@ -9,6 +10,23 @@ const siteMetadata = require('../siteMetadata/siteMetadata')
 
 const App = ({ Component, pageProps }) => {
   return (
+    <>
+
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XC7CR2VRDF"/>
+    <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-XC7CR2VRDF');
+        `,
+        }}
+    />
+
     <ThemeProvider attribute='class' defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content='width=device-width, initial-scale=1' name='viewport' />
@@ -19,6 +37,7 @@ const App = ({ Component, pageProps }) => {
         </LayoutWrapper>
       </TinaProvider>
     </ThemeProvider>
+    </>
   )
 }
 
