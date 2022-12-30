e2d22034ed42f5cd880f10f159071cbf34dcdbe3
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2022-12-30 08:25:21 +0100

    .

--- siteMetadata/headerNavLinks.js
+++ siteMetadata/headerNavLinks.js
@@ -1,5 +1,5 @@
 const headerNavLinks = [
-  { href: 'https://viewer-map-nine.vercel.app/', title: 'playing Maps' }
+  { href: 'https://catalonia-map-viewer.vercel.app/', title: 'playing Maps' }
   /*{ href: '/whoami', title: 'about' },*/
   /*{ href: '/resources', title: 'resources' },
   { href: '/about', title: 'About' }, */
