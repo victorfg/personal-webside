041107dafb3ca5d6ca9504f6607db09e792f6a42
victorfg <victor.fernandez.gayan@gmail.com>
2023-01-16 12:51:19 +0100

    .

--- siteMetadata/headerNavLinks.js
+++ siteMetadata/headerNavLinks.js
@@ -1,8 +1,6 @@
 const headerNavLinks = [
-  { href: 'https://catalonia-map-viewer.vercel.app/', title: 'playing Maps' }
+  { href: "https://catalonia-map-viewer.vercel.app/", title: "playing Maps" },
   /*{ href: '/whoami', title: 'about' },*/
-  /*{ href: '/resources', title: 'resources' },
-  { href: '/about', title: 'About' }, */
-]
+];
 
-export default headerNavLinks
+export default headerNavLinks;
